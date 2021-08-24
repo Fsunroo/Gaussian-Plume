119c42cfbb51c4218d1a7e9771f7427fc890c914
mohammad farhadi <fsunroo@gmail.com>
2021-08-24 10:00:27 +0430

    final

---
+++ ui.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.input_layout = QtWidgets.QVBoxLayout()
+        self.input_layout.setObjectName("input_layout")
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.input_layout.addLayout(self.formLayout)
+        self.horizontalLayout.addLayout(self.input_layout)
+        self.result_layout = QtWidgets.QVBoxLayout()
+        self.result_layout.setObjectName("result_layout")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setAutoFillBackground(True)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.image = QtWidgets.QLabel(self.frame)
+        self.image.setGeometry(QtCore.QRect(70, 80, 231, 271))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.image.sizePolicy().hasHeightForWidth())
+        self.image.setSizePolicy(sizePolicy)
+        self.image.setText("")
+        self.image.setPixmap(QtGui.QPixmap("/home/fhd/Downloads/Telegram Desktop/photo_2021-07-14_22-00-22.jpg"))
+        self.image.setScaledContents(True)
+        self.image.setObjectName("image")
+        self.result_layout.addWidget(self.frame)
+        self.horizontalLayout.addLayout(self.result_layout)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setObjectName("progressBar")
+        self.verticalLayout.addWidget(self.progressBar)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        
+        self.pushButton.clicked.connect(self.click_actions)
+
+
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.pushButton.setText(_translate("MainWindow", "calculate!"))
+
+    def click_actions(self):
+        print('start')
+        from model_gauss import gauss_model
+        from PyQt5.QtGui import QImage
+        model = gauss_model()
+        model.output = model.PLAN_VIEW
+        model.run(self.progressBar)
+        size = model.canvas.size()
+        width, height = size.width(), size.height()
+        im = QImage(model.canvas.buffer_rgba(), width, height, QImage.Format_ARGB32)
+        self.image.setPixmap(QtGui.QPixmap(im))
+        self.image.show()
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
